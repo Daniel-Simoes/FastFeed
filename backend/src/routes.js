d354b1501004978b5211d5473b1b6724bcf6fdba
daniel.simoes <daniel.simoes@hotmail.com>
2020-03-02 16:57:32 +0000

    Test - loading User's model

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,15 @@
 import { Router } from 'express';
+import User from './app/models/User';
+
 const routes = new Router();
 
-routes.get('/', (req, res) =>{
-    return res.json({message:'oi mundo!'});
-})
+routes.get('/', async (req, res) =>{
+    const user = await User.create({
+      name: 'Daniel Simões',
+      email: 'daniel.simoes@hotmail.com',
+      password_hash: 'dt123456',
+    });
+    return res.json(user);
+});
 
 export default routes;
